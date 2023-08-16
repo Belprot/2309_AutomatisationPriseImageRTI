5b049c4e99032ecbeba24166b4c56e9130f38dbd
Meven Ricchieri <74274472+Belprot@users.noreply.github.com>
2023-08-16 18:30:14 +0200

    End of day

---
+++ soft/First/firmware/First.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=First.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/First.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=first.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/first.x.tar
